a4b0b0463cfdfd74d69bbf793ce43c89f4e5c9e0
kirt28 <65890490+kirt28@users.noreply.github.com>
2021-01-20 13:45:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,3 @@
-provider "aws" {
-}
-
 resource "aws_s3_bucket" "b" {
   bucket = "my-tf-test-bucket-09152020"
   acl    = "private"
@@ -8,6 +5,13 @@ resource "aws_s3_bucket" "b" {
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
+  }
+}
+terraform {
+  backend "s3" {
+    bucket = "top-bar-1"
+    key    = ""
+    region = "us-east-1"
   }
 }
 
